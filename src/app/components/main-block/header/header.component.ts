3ff84b77d126e4752c957e535998e97f9e9fcaa1
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2024-02-09 21:25:54 +0200

    creat components header, footer

---
+++ src/app/components/main-block/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bsa-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
