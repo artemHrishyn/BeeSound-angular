29aba340bcdfa39a10a8b2ea7a5439c2518ec23d
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2024-02-26 22:14:47 +0200

    add main pages

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminPanelComponent } from '../components/admin-panel/admin-panel.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
-  // { path: '', redirectTo: 'main', pathMatch: 'full' },
-  // { path: "main", component: MainComponent },
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
+  { path: "main", component: MainComponent },
   {path: 'admin', component:AdminPanelComponent}
 ];
 
