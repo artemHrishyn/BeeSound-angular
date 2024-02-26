a8e9a6fbef94482b23d41850dd1b751b286a886c
Artem Hrishyn <artem.hrishyn.it@gmail.com>
2024-02-26 22:09:04 +0200

    add admin panel

---
+++ src/app/components/admin-panel/admin-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bsa-admin-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-panel.component.html',
+  styleUrl: './admin-panel.component.scss'
+})
+export class AdminPanelComponent {
+
+}
---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdminPanelComponent } from '../components/admin-panel/admin-panel.component';
+
+export const routes: Routes = [
+  // { path: '', redirectTo: 'main', pathMatch: 'full' },
+  // { path: "main", component: MainComponent },
+  {path: 'admin', component:AdminPanelComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule { }
